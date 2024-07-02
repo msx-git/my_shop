543adad1f46062ce08841bfefec48005eea151d9
Musoxon <msx.tuit@gmail.com>
2024-07-02 16:14:32 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my_shop
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      useInheritedMediaQuery: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return const MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Text('Hello World!'),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
